4427fb219c51094323f211f6522b99d309cf04da
Sanat1427 <sanatkishore04@gmail.com>
2025-10-24 23:37:56 +0530

    Fix COOP warning in Next.js headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-images:{
-    domains:['lh3.googleusercontent.com']
-}
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
